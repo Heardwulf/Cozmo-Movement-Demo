70513ebb418796993a88d28dc76c8f20490b2e67
Abdalrhman Mohamed <abdalrhman-mohamed@uiowa.edu>
2019-01-17 11:15:06 -0600

    variation 4

---
+++ Variation4.py
@@ -0,0 +1,7 @@
+import cozmo;
+
+def cozmo_program(robot: cozmo.robot.Robot):
+    robot.say_text("variation 4").wait_for_completed()
+
+if __name__ == "__main__":
+    cozmo.run_program(cozmo_program)
